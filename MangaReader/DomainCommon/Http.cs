29737a212d26b964a0540d7e1e78597cd7813269
Nguyendat09092004 <nguyendat09092004@gmail.com>
2024-12-12 15:37:19 +0700

    123

---
+++ MangaReader/DomainCommon/Http.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MangaReader.DomainCommon
+{
+    public class Http : IDisposable
+    {
+        private readonly HttpClient client = new();
+
+        public Http()
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "MangaReader");
+        }
+
+        public Task<string> GetStringAsync(string url)
+        {
+            return GetStringAsync(url, CancellationToken.None);
+        }
+
+        public async Task<string> GetStringAsync(string url, CancellationToken token)
+        {
+            try
+            {
+                return await client.GetStringAsync(url, token);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new NetworkException(ex.Message);
+            }
+        }
+
+        public Task<byte[]> GetByteArrayAsync(string url)
+        {
+            return GetByteAsync(url, CancellationToken.None);
+        }
+
+        public async Task<byte[]> GetByteAsync(string url, CancellationToken token)
+        {
+            try
+            {
+                using var message = await client.GetAsync(url, token);
+                return await message.Content.ReadAsByteArrayAsync(token);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new NetworkException(ex.Message);
+            }
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+    
+}
---
+++ MangaReader/ViewCommon/NewErrorPanel.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MangaReader.ViewCommon;
+
+public partial class NewErrorPanel : UserControl
+{
+    public NewErrorPanel()
+    {
+        InitializeComponent();
+    }
+}
